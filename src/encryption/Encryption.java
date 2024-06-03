c3727bffccda75264470c9205b7267137664afd9
Trebush <wrzosowskyy@gmail.com>
2024-06-03 21:19:50 +0200

    test

--- src/encryption/Encryption.java
+++ src/encryption/Encryption.java
@@ -3,7 +3,7 @@ package encryption;
 public class Encryption {
     private String test;
     public String test(){
-        this.test = "test";
+        this.test = "test and test";
         return test;
     }
 }
